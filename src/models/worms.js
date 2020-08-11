c37182e756aa4fc51b29ec6aba969743ee1324ad
ehpc <ehpc@ehpc.io>
2020-08-11 22:25:18 +0300

    First draft

---
+++ src/models/worms.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: 'Worm 1',
+    size: 200,
+    age: 524,
+  },
+  {
+    name: 'Worm 2',
+    size: 350,
+    age: 2403,
+  },
+  {
+    name: 'Worm 3',
+    size: 320,
+    age: 1350,
+  },
+  {
+    name: 'Worm 4',
+    size: 400,
+    age: 902,
+  },
+  {
+    name: 'Worm 5',
+    size: 365,
+    age: 3040,
+  },
+  {
+    name: 'Worm 6',
+    size: 310,
+    age: 2981,
+  },
+  {
+    name: 'Worm 7',
+    size: 290,
+    age: 1299,
+  },
+  {
+    name: 'Worm 8',
+    size: 245,
+    age: 4030,
+  },
+];
